30a45f192181a6e1fa646e171fc0058c373326b6
Benjamin <59982887+Benji-c-Ohanu@users.noreply.github.com>
2024-03-21 18:52:28 +0100

    initial commit

---
+++ src/components/array.js
@@ -0,0 +1,19 @@
+const array = [
+  {
+      id: "1",
+      Name: "Sarah",
+      Age: "27",
+  },
+  {
+      id: "2",
+      Name: "Benjamin",
+      Age: "45 ",
+  },
+  {
+      id: "3",
+      Name: "David",
+      Age: "32",
+  },
+];
+
+export default array;
